22e27a0fea180fd2aefa28da9c59d9bc2228a99e
Graham Enos <graham@strangeworks.com>
2025-05-08 14:30:11 -0400

    decouple lexing & execution with a new `Word` enum

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@
 #![deny(unsafe_code)]
 use indexmap::IndexMap;
 use logos::Logos;
-use std::{cell::RefCell, fmt, num::ParseIntError};
+use std::{cell::RefCell, convert::TryFrom, fmt, num::ParseIntError};
 
 /// Our Error type.
 #[derive(Clone, Debug, Default, PartialEq, Eq, thiserror::Error)]
@@ -17,15 +17,15 @@ pub enum Error {
     Bad,
     /// I expected a number, but I found `{0}`.
     #[error("I expected a number, but I found `{0}`.")]
-    NaN(Token),
+    NaN(Word),
     /// The stack is too small for `{0}`; it requires {1}, but the stack only has {2}.
     #[error("The stack is too small for `{0}`; it requires {1}, but the stack only has {2}.")]
-    Small(Token, usize, usize),
+    Small(Word, usize, usize),
     /// Parsing error: `{0}`.
     #[error("Parsing error: `{0}`.")]
     Parsing(String),
-    /// Something happend when trying to read: {0}.
-    #[error("Something happend when trying to read: {0}.")]
+    /// Something happened with readline: `{0}`.
+    #[error("Something happened with readline: `{0}`.")]
     Readline(String),
     /// Unknown op: `{0}`.
     #[error("Unknown op: `{0}`.")]
@@ -39,9 +39,9 @@ pub enum Error {
     /// `def` needs a name, but none was supplied.
     #[error("`def` needs a name, but none was supplied.")]
     DefName,
-    /// `def` needs a name, but a number `{0}` was supplied.
-    #[error("`def` needs a name, but a number `{0}` was supplied.")]
-    DefNum(i64),
+    /// A name was expected, but a number `{0}` was supplied.
+    #[error("A name was expected, but a number `{0}` was supplied.")]
+    NumNotName(i64),
     /// `def` needs a body, but none was supplied.
     #[error("`def` needs a body, but none was supplied.")]
     DefBody,
@@ -51,52 +51,54 @@ pub enum Error {
 #[derive(Logos, Debug, PartialEq, Eq, Clone)]
 #[logos(error = Error)]
 #[logos(skip r"\s")]
-pub enum Token {
+pub enum Token<'source> {
     /// Define a new word.
     #[token("def")]
     Def,
-    #[token("pop")]
-    /// Pop an item off the stack.
-    Pop,
-    #[token("swap")]
-    /// Swap the top two elements of the stack.
-    Swap,
-    #[token("dup")]
-    /// Duplicate the first element of the stack.
-    Dup,
-    #[token("add")]
-    /// Add the first two elements of the stack.
-    Add,
-    #[token("sub")]
-    /// Subtract the second from the first element of the stack.
-    Sub,
-    /// Multiply the first two elements of the stack.
-    #[token("mul")]
-    Mul,
-    #[token("div")]
-    /// Divide the second into the first element of the stack.
-    Div,
-    #[token("mod")]
-    /// Take the remainder of the second in the first element of the stack.
-    Mod,
-    #[token("zero?")]
-    /// Pop 3 elements. If the first is zero, push the second back on; otherwise, push the third.
-    Zero,
+    /// A core word.
+    #[regex(r"(pop|swap|dup|add|sub|mul|div|mod|zero)")]
+    Core(&'source str),
     /// An integer in decimal notation.
-    #[regex(r"-?[0-9]+", |lex| lex.slice().parse().map_err(|e: ParseIntError| Error::Parsing(e.to_string())), priority = 3)]
+    #[regex(r"-?[0-9]+", |lex| lex.slice().parse().map_err(|e: ParseIntError| Error::Parsing(e.to_string())))]
     Num(i64),
     /// An integer in hexadecimal notation.
     #[regex(r"#[0-9a-fA-F]+", |lex| i64::from_str_radix(&lex.slice()[1..], 16).map_err(|e: ParseIntError| Error::Parsing(e.to_string())))]
     Hex(i64),
-    /// A (possibly unknown) word.
-    #[regex(r"\S+", |lex| lex.slice().to_owned())]
-    Word(String),
+    /// A (possibly unknown) custom token.
+    #[regex(r"\S+", priority = 0)]
+    Custom(&'source str),
 }
 
-impl fmt::Display for Token {
+/// The words upon which our stack machine works.
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub enum Word {
+    /// Pop an item off the stack.
+    Pop,
+    /// Swap the top two elements of the stack.
+    Swap,
+    /// Duplicate the first element of the stack.
+    Dup,
+    /// Add the first two elements of the stack.
+    Add,
+    /// Subtract the second from the first element of the stack.
+    Sub,
+    /// Multiply the first two elements of the stack.
+    Mul,
+    /// Divide the second into the first element of the stack.
+    Div,
+    /// Take the remainder of the second in the first element of the stack.
+    Mod,
+    /// Pop 3 elements. If the first is zero, push the second back on; otherwise, push the third.
+    Zero,
+    /// An integer.
+    Num(i64),
+    /// A custom word.
+    Custom(String),
+}
+
+impl fmt::Display for Word {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            Self::Def => f.write_str("def"),
             Self::Pop => f.write_str("pop"),
             Self::Swap => f.write_str("swap"),
             Self::Dup => f.write_str("dup"),
@@ -107,25 +109,51 @@ impl fmt::Display for Token {
             Self::Mod => f.write_str("mod"),
             Self::Zero => f.write_str("zero?"),
             Self::Num(n) => write!(f, "{n}"),
-            Self::Hex(n) => write!(f, "#{n:x}"),
-            Self::Word(w) => write!(f, "{w}"),
+            Self::Custom(w) => write!(f, "{w}"),
         }
     }
 }
 
-impl Token {
+impl TryFrom<Token<'_>> for Word {
+    type Error = Error;
+    fn try_from(t: Token<'_>) -> Result<Self, Self::Error> {
+        match t {
+            Token::Def => Err(Error::Reserved),
+            Token::Core(w) => match w {
+                s if s == Self::Pop.to_string() => Ok(Self::Pop),
+                s if s == Self::Swap.to_string() => Ok(Self::Swap),
+                s if s == Self::Dup.to_string() => Ok(Self::Dup),
+                s if s == Self::Add.to_string() => Ok(Self::Add),
+                s if s == Self::Sub.to_string() => Ok(Self::Sub),
+                s if s == Self::Mul.to_string() => Ok(Self::Mul),
+                s if s == Self::Div.to_string() => Ok(Self::Div),
+                s if s == Self::Mod.to_string() => Ok(Self::Mod),
+                s if s == Self::Zero.to_string() => Ok(Self::Zero),
+                // Should be unreachable, but let's be careful.
+                _ => Err(Error::Unknown(w.to_string())),
+            },
+            Token::Num(n) | Token::Hex(n) => Ok(Self::Num(n)),
+            Token::Custom(w) => Ok(Self::Custom(w.to_string())),
+        }
+    }
+}
+
+impl Word {
     fn into_name(self) -> Result<String, Error> {
         match self {
-            Self::Word(w) => Ok(w),
-            Self::Def => Err(Error::Reserved),
-            Self::Num(n) | Self::Hex(n) => Err(Error::DefNum(n)),
+            Self::Num(n) => Err(Error::NumNotName(n)),
+            Self::Custom(w) => Ok(w.to_string()),
             _ => Ok(self.to_string()),
         }
     }
-    fn into_num(self) -> Result<i64, Error> {
-        match self {
-            Self::Num(n) | Self::Hex(n) => Ok(n),
-            _ => Err(Error::NaN(self)),
+}
+
+impl TryFrom<Word> for i64 {
+    type Error = Error;
+    fn try_from(w: Word) -> Result<Self, Self::Error> {
+        match w {
+            Word::Num(n) => Ok(n),
+            _ => Err(Error::NaN(w)),
         }
     }
 }
@@ -133,24 +161,23 @@ impl Token {
 /// The main data structure: a stack machine with an environment of local definitions.
 #[derive(Default)]
 pub struct Machine {
-    env: RefCell<IndexMap<String, Vec<Token>>>,
-    stack: RefCell<Vec<Token>>,
+    env: RefCell<IndexMap<String, Vec<Word>>>,
+    stack: RefCell<Vec<Word>>,
 }
 
 impl fmt::Display for Machine {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         f.write_str("core:")?;
         for t in [
-            Token::Def,
-            Token::Pop,
-            Token::Swap,
-            Token::Dup,
-            Token::Add,
-            Token::Sub,
-            Token::Mul,
-            Token::Div,
-            Token::Mod,
-            Token::Zero,
+            Word::Pop,
+            Word::Swap,
+            Word::Dup,
+            Word::Add,
+            Word::Sub,
+            Word::Mul,
+            Word::Div,
+            Word::Mod,
+            Word::Zero,
         ] {
             write!(f, " {t}")?;
         }
@@ -167,60 +194,59 @@ impl fmt::Display for Machine {
 }
 
 impl Machine {
-    fn pop(&self, t: &Token, required: usize, stack_len: usize) -> Result<Token, Error> {
+    fn pop(&self, t: &Word, required: usize, stack_len: usize) -> Result<Word, Error> {
         self.stack
             .borrow_mut()
             .pop()
             .ok_or_else(|| Error::Small(t.clone(), required, stack_len))
     }
-    fn eval(&self, t: &Token) -> Result<(), Error> {
+    fn eval(&self, t: &Word) -> Result<(), Error> {
         match t {
-            Token::Def => return Err(Error::Reserved),
-            Token::Num(_) | Token::Hex(_) => self.stack.borrow_mut().push(t.clone()),
-            Token::Pop => self.pop(t, 1, 0).map(|_| ())?,
-            Token::Swap => {
+            Word::Num(_) => self.stack.borrow_mut().push(t.clone()),
+            Word::Pop => self.pop(t, 1, 0).map(|_| ())?,
+            Word::Swap => {
                 let x = self.pop(t, 2, 0)?;
                 let y = self.pop(t, 2, 1)?;
                 self.stack.borrow_mut().push(x);
                 self.stack.borrow_mut().push(y);
             }
-            Token::Dup => {
+            Word::Dup => {
                 let x = self.pop(t, 1, 0)?;
                 self.stack.borrow_mut().push(x.clone());
                 self.stack.borrow_mut().push(x);
             }
-            Token::Add => {
-                let x = self.pop(t, 2, 0).and_then(Token::into_num)?;
-                let y = self.pop(t, 2, 1).and_then(Token::into_num)?;
-                self.stack.borrow_mut().push(Token::Num(x + y));
+            Word::Add => {
+                let x = self.pop(t, 2, 0).and_then(i64::try_from)?;
+                let y = self.pop(t, 2, 1).and_then(i64::try_from)?;
+                self.stack.borrow_mut().push(Word::Num(x + y));
             }
-            Token::Sub => {
-                let x = self.pop(t, 2, 0).and_then(Token::into_num)?;
-                let y = self.pop(t, 2, 1).and_then(Token::into_num)?;
-                self.stack.borrow_mut().push(Token::Num(x - y));
+            Word::Sub => {
+                let x = self.pop(t, 2, 0).and_then(i64::try_from)?;
+                let y = self.pop(t, 2, 1).and_then(i64::try_from)?;
+                self.stack.borrow_mut().push(Word::Num(x - y));
             }
-            Token::Mul => {
-                let x = self.pop(t, 2, 0).and_then(Token::into_num)?;
-                let y = self.pop(t, 2, 1).and_then(Token::into_num)?;
-                self.stack.borrow_mut().push(Token::Num(x * y));
+            Word::Mul => {
+                let x = self.pop(t, 2, 0).and_then(i64::try_from)?;
+                let y = self.pop(t, 2, 1).and_then(i64::try_from)?;
+                self.stack.borrow_mut().push(Word::Num(x * y));
             }
-            Token::Div => {
-                let x = self.pop(t, 2, 0).and_then(Token::into_num)?;
-                let y = self.pop(t, 2, 1).and_then(Token::into_num)?;
-                self.stack.borrow_mut().push(Token::Num(x / y));
+            Word::Div => {
+                let x = self.pop(t, 2, 0).and_then(i64::try_from)?;
+                let y = self.pop(t, 2, 1).and_then(i64::try_from)?;
+                self.stack.borrow_mut().push(Word::Num(x / y));
             }
-            Token::Mod => {
-                let x = self.pop(t, 2, 0).and_then(Token::into_num)?;
-                let y = self.pop(t, 2, 1).and_then(Token::into_num)?;
-                self.stack.borrow_mut().push(Token::Num(x % y));
+            Word::Mod => {
+                let x = self.pop(t, 2, 0).and_then(i64::try_from)?;
+                let y = self.pop(t, 2, 1).and_then(i64::try_from)?;
+                self.stack.borrow_mut().push(Word::Num(x % y));
             }
-            Token::Zero => {
-                let x = self.pop(t, 3, 0).and_then(Token::into_num)?;
+            Word::Zero => {
+                let x = self.pop(t, 3, 0).and_then(i64::try_from)?;
                 let y = self.pop(t, 3, 1)?;
                 let z = self.pop(t, 3, 2)?;
                 self.stack.borrow_mut().push(if x == 0 { y } else { z });
             }
-            Token::Word(w) => match self.env.borrow().get(w) {
+            Word::Custom(w) => match self.env.borrow().get(w) {
                 None => return Err(Error::Unknown(w.to_string())),
                 Some(vs) => {
                     for v in vs {
@@ -240,8 +266,12 @@ impl Machine {
         while let Some(t) = ts.next() {
             match t {
                 Token::Def => {
-                    let k = ts.next().ok_or(Error::DefName).and_then(Token::into_name)?;
-                    let us = ts.collect::<Vec<_>>();
+                    let k = ts
+                        .next()
+                        .ok_or(Error::DefName)
+                        .and_then(Word::try_from)
+                        .and_then(Word::into_name)?;
+                    let us = ts.map(Word::try_from).collect::<Result<Vec<_>, _>>()?;
                     if us.is_empty() {
                         return Err(Error::DefBody);
                     } else if us.iter().any(|u| u.to_string() == k) {
@@ -250,7 +280,7 @@ impl Machine {
                     let _ = self.env.borrow_mut().insert(k, us);
                     break;
                 }
-                _ => self.eval(&t)?,
+                _ => self.eval(&Word::try_from(t)?)?,
             }
         }
         Ok(())
