4f97fa9a5cc2cf5e307c7560f70fc7a8fd559e7b
Graham Enos <graham@strangeworks.com>
2025-05-08 16:28:42 -0400

    squeeze like a sponge

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@
 #![deny(missing_docs)]
 #![deny(unsafe_code)]
 use drsm::{Error, Machine};
-use rustyline::{Config, EditMode, Editor, error::ReadlineError};
+use rustyline::{Config, DefaultEditor, EditMode, error::ReadlineError};
 
 /// Errors that could occur while using the REPL.
 #[derive(Debug, thiserror::Error)]
@@ -19,8 +19,8 @@ pub enum ReplError {
 }
 
 fn main() -> Result<(), ReplError> {
-    let config = Config::builder().edit_mode(EditMode::Vi).build();
-    let mut rl: Editor<(), _> = Editor::with_config(config).map_err(ReplError::Readline)?;
+    let c = Config::builder().edit_mode(EditMode::Vi).build();
+    let mut r = DefaultEditor::with_config(c).map_err(ReplError::Readline)?;
     let m = Machine::default();
     println!(
         r"
@@ -31,13 +31,13 @@ fn main() -> Result<(), ReplError> {
 /_____/_/ |_|/____/_/  /_/
 "
     );
-    if rl.load_history("history.txt").is_err() {
+    if r.load_history("history.txt").is_err() {
         println!("No previous history.");
     }
     loop {
-        match rl.readline(">  ") {
+        match r.readline(">  ") {
             Ok(l) => {
-                rl.add_history_entry(&l).map_err(ReplError::Readline)?;
+                r.add_history_entry(&l).map_err(ReplError::Readline)?;
                 match m.read_eval(&l) {
                     Ok(()) => {}
                     Err(e) => eprintln!("{e}"),
@@ -56,6 +56,7 @@ fn main() -> Result<(), ReplError> {
         }
         println!("{m}");
     }
-    rl.save_history("history.txt").map_err(ReplError::Readline)?;
+    r.save_history("history.txt")
+        .map_err(ReplError::Readline)?;
     Ok(())
 }
