947603352b60299ecdcbd17ea6a351557be2e385
Graham Enos <graham@strangeworks.com>
2025-05-13 12:22:36 -0400

    👻 UNSAFE STACK OPERATIONS 👻

--- src/machine.rs
+++ src/machine.rs
@@ -1,13 +1,23 @@
+#![allow(unsafe_code)]
 use crate::{error::Error, token::Token, word::Word};
 use indexmap::IndexMap;
 use logos::Logos;
 use std::{convert::TryFrom, fmt};
 
 /// The main data structure: a stack machine with an environment of local definitions.
-#[derive(Default, Debug)]
+#[derive(Debug)]
 pub struct Machine {
     env: IndexMap<String, Vec<Word>>,
     stack: Vec<i64>,
+}
+
+impl Default for Machine {
+    fn default() -> Self {
+        Self {
+            env: IndexMap::with_capacity(1_024),
+            stack: Vec::with_capacity(1024),
+        }
+    }
 }
 
 impl fmt::Display for Machine {
@@ -67,24 +77,24 @@ impl Machine {
         }
         Ok(())
     }
-    fn eval(&mut self, w: &Word) -> Result<(), Error> {
-        self.check(w)?;
-        eval_inner(&self.env, &mut self.stack, w)
+    fn eval(&mut self, word: &Word) -> Result<(), Error> {
+        self.check(word)?;
+        eval_inner(&self.env, &mut self.stack, word)
     }
-    fn check(&self, w: &Word) -> Result<(), Error> {
+    fn check(&self, word: &Word) -> Result<(), Error> {
         let s = self.stack.len();
-        let r = match w {
+        let r = match word {
             Word::Num(_) | Word::Custom(_) => 0,
             Word::Pop | Word::Dup => 1,
             Word::Swap | Word::Add | Word::Sub | Word::Mul | Word::Div | Word::Mod => 2,
             Word::Zero => 3,
         };
         if s < r {
-            Err(Error::Small(w.to_string(), r, s))
-        } else if (*w == Word::Div || *w == Word::Mod) && self.stack[s - 2] == 0 {
-            Err(Error::NNZ(w.to_string()))
-        } else if matches!(w, Word::Custom(_)) && !self.env.contains_key(&w.to_string()) {
-            Err(Error::Unknown(w.to_string()))
+            Err(Error::Small(word.to_string(), r, s))
+        } else if (*word == Word::Div || *word == Word::Mod) && self.stack[s - 2] == 0 {
+            Err(Error::NNZ(word.to_string()))
+        } else if matches!(word, Word::Custom(_)) && !self.env.contains_key(&word.to_string()) {
+            Err(Error::Unknown(word.to_string()))
         } else {
             Ok(())
         }
@@ -92,61 +102,58 @@ impl Machine {
 }
 
 /// Broken out to untangle mutability concerns.
-/// Full of `stack.pop().expect(…)` because this should only be called from within `Machine::eval`.
+/// Full of **UNSAFE** code because this should only be called from within `Machine::eval`.
 fn eval_inner(
     env: &IndexMap<String, Vec<Word>>,
     stack: &mut Vec<i64>,
-    w: &Word,
+    word: &Word,
 ) -> Result<(), Error> {
-    match w {
-        Word::Pop => {
-            stack.pop().expect("Internal error @ pop");
+    let n = stack.len();
+    match word {
+        Word::Pop => unsafe { stack.set_len(n - 1) },
+        Word::Swap => stack.swap(n - 1, n - 2),
+        Word::Dup => stack.push(unsafe { *stack.get_unchecked(n - 1) }),
+        Word::Add => unsafe {
+            *stack.get_unchecked_mut(n - 2) = stack
+                .get_unchecked(n - 1)
+                .wrapping_add(*stack.get_unchecked(n - 2));
+            stack.set_len(n - 1);
+        },
+        Word::Sub => unsafe {
+            *stack.get_unchecked_mut(n - 2) = stack
+                .get_unchecked(n - 1)
+                .wrapping_sub(*stack.get_unchecked(n - 2));
+            stack.set_len(n - 1);
+        },
+        Word::Mul => unsafe {
+            *stack.get_unchecked_mut(n - 2) = stack
+                .get_unchecked(n - 1)
+                .wrapping_mul(*stack.get_unchecked(n - 2));
+            stack.set_len(n - 1);
+        },
+        Word::Div => unsafe {
+            *stack.get_unchecked_mut(n - 2) = stack
+                .get_unchecked(n - 1)
+                .wrapping_div(*stack.get_unchecked(n - 2));
+            stack.set_len(n - 1);
+        },
+        Word::Mod => unsafe {
+            *stack.get_unchecked_mut(n - 2) = stack
+                .get_unchecked(n - 1)
+                .wrapping_rem(*stack.get_unchecked(n - 2));
+            stack.set_len(n - 1);
+        },
+        Word::Zero => unsafe {
+            if *stack.get_unchecked(n - 1) == 0 {
+                stack.swap(n - 2, n - 3);
+            }
+            stack.set_len(n - 2);
+        },
+        Word::Num(x) => {
+            stack.push(*x);
         }
-        Word::Swap => {
-            let x = stack.pop().expect("Internal error @ swap 1");
-            let y = stack.pop().expect("Internal error @ swap 2");
-            stack.push(x);
-            stack.push(y);
-        }
-        Word::Dup => {
-            let x = stack.pop().expect("Internal error @ dup");
-            stack.push(x);
-            stack.push(x);
-        }
-        Word::Add => {
-            let x = stack.pop().expect("Internal error @ add 1");
-            let y = stack.pop().expect("Internal error @ add 2");
-            stack.push(x.wrapping_add(y));
-        }
-        Word::Sub => {
-            let x = stack.pop().expect("Internal error @ sub 1");
-            let y = stack.pop().expect("Internal error @ sub 2");
-            stack.push(x.wrapping_sub(y));
-        }
-        Word::Mul => {
-            let x = stack.pop().expect("Internal error @ mul 1");
-            let y = stack.pop().expect("Internal error @ mul 2");
-            stack.push(x.wrapping_mul(y));
-        }
-        Word::Div => {
-            let x = stack.pop().expect("Internal error @ div 1");
-            let y = stack.pop().expect("Internal error @ div 2");
-            stack.push(x.wrapping_div(y));
-        }
-        Word::Mod => {
-            let x = stack.pop().expect("Internal error @ mod 1");
-            let y = stack.pop().expect("Internal error @ mod 2");
-            stack.push(x.wrapping_rem(y));
-        }
-        Word::Zero => {
-            let x = stack.pop().expect("Internal error at zero? 1");
-            let y = stack.pop().expect("Internal error at zero? 2");
-            let z = stack.pop().expect("Internal error at zero? 3");
-            stack.push(if x == 0 { y } else { z });
-        }
-        Word::Num(n) => stack.push(*n),
-        Word::Custom(w) => {
-            for v in env.get(w).ok_or_else(|| Error::Unknown(w.to_string()))? {
+        Word::Custom(c) => {
+            for v in &env[c] {
                 eval_inner(env, stack, v)?;
             }
         }
